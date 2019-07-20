3347c35cbf5c98ced66659c1cba21e07eb375dd3
Karoene <karoene.d@hotmail.com>
2019-07-19 23:17:26 -0300

    aades
    added

---
+++ main.py
@@ -0,0 +1,25 @@
+from analisador import AnalisadorLexico
+#import { Parser } from './parser'
+
+source = 'z=((x+y)-((x+y)/(x-y)))+((x+y)/(x-y))"kakakak"'
+source1 = 'k = esfes'
+source2 = ''
+lines = source.split('\n') # separa as linhas da entrada do codigo fonte, caso aja mais de uma linha no source
+
+#print(lines[1])
+tokens = []   #lista de todos os tokens do codigo fonte
+#for (let i = 0; i < lines.length; ++i) {  
+# #percorre todas as linhas do codigo fonte
+for indice,linha in enumerate(lines):
+    lexer = AnalisadorLexico(linha, indice+1 )  #nova instancia, passando conteudo da linha lida, e numero da linha 
+    #print(lexer)
+    curTokens = lexer.tokenize()  #pega a lista com todos os tokens da linha lida
+    #print(curTokens)
+    
+    if linha:  #se a linha atual nao for vazia
+        #for (let j = 0; j < curTokens.length; ++j) //percorre todos os tokens da linha atual
+        for token in curTokens:
+            tokens.append(token); #insere na lista geral de tokens os tokens dessa linha
+
+for token in tokens:
+  print(token)
